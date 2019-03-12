56a1bf53ef5b215c34998aa3d455482b3d084ebe
Dmytro <dmtholovko@gmail.com>
2019-03-12 16:48:09 +0200

    update Category.swift

--- spendserIOS/Category.swift
+++ spendserIOS/Category.swift
@@ -7,3 +7,17 @@
 //
 
 import Foundation
+
+class Category {
+    let ID : Int
+    let name : String
+    let type : Bool
+    let createdDate : Date
+    
+    init(id : Int, name : String, type: Bool, date: Date) {
+        self.ID = id
+        self.name = name
+        self.type = type
+        self.createdDate = date
+    }
+}
