c53eb258ad7e1582f248fe2680e696e7abe0bcd1
Dmytro <dmtholovko@gmail.com>
2019-03-12 16:42:24 +0200

    update User.swift

--- spendserIOS/User.swift
+++ spendserIOS/User.swift
@@ -7,3 +7,19 @@
 //
 
 import Foundation
+
+class User {
+    let ID : Int
+    var username : String
+    var password : String
+    var balance : Float
+    let createdDate : Date
+    
+    init(id : Int, username : String, password : String, balance : Float, date : Date) {
+        self.ID = id
+        self.username = username
+        self.password = password
+        self.balance = balance
+        self.createdDate = date
+    }
+}
