6f5115cc55babc329e574c2cb4268c618b52b504
Thongvor <dmtholovko@gmail.com>
2019-04-25 18:24:45 +0300

    add ticket url

--- spendsterIOS/Presentation/HomeScreen/HomeScreenViewController.swift
+++ spendsterIOS/Presentation/HomeScreen/HomeScreenViewController.swift
@@ -11,7 +11,7 @@ import UIKit
 class HomeScreenViewController: UIViewController {
     @IBAction func logOutButtonPressed(_ sender: Any) {
         // TODO remove loguot button from main screen and add it to profile screen
-        // ticket:
+        // ticket: https://trello.com/c/ck0DDnn8/127-profile-screen
         UserDefaults.standard.set(false, forKey: "alreadyLoggedIn")
         dismiss(animated: true, completion: nil)
     }
