cbd6cea34015656b6a7e74e7ae05af7b8991365e
Thongvor <dmtholovko@gmail.com>
2019-04-25 17:33:22 +0300

    fix regex of phone number

--- spendsterIOS/Presentation/Auth/Validation/ValidationPattern.swift
+++ spendsterIOS/Presentation/Auth/Validation/ValidationPattern.swift
@@ -11,6 +11,6 @@ struct ValidationPattern {
     static let email = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
     static let password = "[A-Z0-9a-z._%+-]{8,24}"
     static let name = "[A-Za-z]+\\ [A-Za-z]{1,64}"
-    static let phone = "[+]+[0-9]{10,15}"
+    static let phone = "[+][0-9]{10,15}"
     static let username = "[A-Z0-9a-z]{4,16}"
 }
