b53d1a896b74551a93bc9c10447703bafef6a99b
Thongvor <dmtholovko@gmail.com>
2019-05-07 18:03:02 +0300

    add to uiviews button functional

--- spendsterIOS/Presentation/Expenses/AddExpensesViewController.swift
+++ spendsterIOS/Presentation/Expenses/AddExpensesViewController.swift
@@ -10,21 +10,32 @@ import UIKit
 
 class AddExpensesViewController: UIViewController {
 
+    @IBOutlet weak var calendar: BorderView!
+    @IBOutlet weak var category: BorderView!
+    @IBOutlet weak var currency: BorderView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        let categoryTap = UITapGestureRecognizer(target: self, action: #selector(self.categoryTapped(_:)))
+        self.category.addGestureRecognizer(categoryTap)
+        
+        let currencyTap = UITapGestureRecognizer(target: self, action: #selector(self.currencyTapped(_:)))
+        self.currency.addGestureRecognizer(currencyTap)
 
-        // Do any additional setup after loading the view.
+        let calendarTap = UITapGestureRecognizer(target: self, action: #selector(self.calendarTapped(_:)))
+        self.calendar.addGestureRecognizer(calendarTap)
+
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @objc func categoryTapped(_ sender: UITapGestureRecognizer) {
+        print("category tapped!!1")
     }
-    */
-
+    
+    @objc func currencyTapped(_ sender: UITapGestureRecognizer) {
+        print("currencyTapped tappedTEPPED YAAAY!!1")
+    }
+    
+    @objc func calendarTapped(_ sender: UITapGestureRecognizer) {
+        print("calendar tapped!!1")
+    }
 }
